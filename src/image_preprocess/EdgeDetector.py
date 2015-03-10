c7d3044bba291aec9456c282e14786f050aa1dfe
Kern Ding <kern.ding@laserfiche.com>
2015-03-09 18:02:28 -0700

    just share the files from different location.

--- src/image_preprocess/EdgeDetector.py
+++ src/image_preprocess/EdgeDetector.py
@@ -7,7 +7,7 @@ from skimage.feature import canny
 __author__ = 'Kern'
 
 
-def image_binarization(image):
+def binarize(image):
     if len(image.shape) > 2:
         image = rgb2grey(image)
 
@@ -20,4 +20,9 @@ def canny_edge_detector(binary_img, sigma):
 
 
 def edge_coordinate(edge_map):
-    return find_contours(edge_map, 0.5, fully_connected='high')
+    ret = []
+
+    for contour in find_contours(edge_map, 0.5, fully_connected='high'):
+        ret.extend(contour)
+
+    return ret
