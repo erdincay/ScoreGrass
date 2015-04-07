79228bd2a6bc7fbff401928e93bf100de71b0e0a
Kern Ding <kern.ding@laserfiche.com>
2015-04-06 18:08:34 -0700

    temp of prediction serialization.

--- src/learning/strategy/RegressionManager.py
+++ src/learning/strategy/RegressionManager.py
@@ -1,7 +1,15 @@
 import os
+import math
 
 from src.learning.evaluation import CrossValidation
 from src.learning.preprocess import Preprocessor
+from src.learning.regression.ScikitLasso import ScikitLasso
+from src.learning.regression.ScikitRidge import ScikitRidge
+from src.learning.regression.ScikitBayesianRidge import ScikitBayesianRidge
+from src.learning.regression.PyBrainANNs import PyBrainANNs
+from src.learning.regression.ScikitMultiTaskLasso import ScikitMultiTaskLasso
+from src.learning.regression.ScikitSvm import ScikitSvm
+from src.learning.regression.TheanetsANNs import TheanetsANNs
 
 
 __author__ = 'Kern'
@@ -13,18 +21,32 @@ def combine_name(parent, self):
     return parent + '_' + self + serialize_extension
 
 
-def feature_dimensions(data_struct):
-    if len(data_struct.shape) >= 2:
-        return data_struct.shape[1]
-    elif len(data_struct.shape) == 1:
-        return 1
+def _register_color_regression(str_parent):
+    return [
+        ScikitLasso(combine_name(str_parent, ScikitLasso.__name__)),
+        ScikitRidge(combine_name(str_parent, ScikitRidge.__name__))
+    ]
 
-    raise TypeError('unknown pandas struct type')
+
+def _register_quality_regression(x_dim, y_dim, str_parent):
+    return [
+        ScikitBayesianRidge(combine_name(str_parent, ScikitBayesianRidge.__name__)),
+        ScikitSvm('rbf', combine_name(str_parent, ScikitSvm.__name__)),
+        TheanetsANNs(x_dim, y_dim, math.floor(x_dim / 2), combine_name(str_parent, TheanetsANNs.__name__)),
+        PyBrainANNs(x_dim, y_dim, math.floor(x_dim / 2), combine_name(str_parent, PyBrainANNs.__name__))
+    ]
+
+
+def _register_mixed_regression(x_dim, y_dim, str_parent):
+    return [
+        ScikitMultiTaskLasso(combine_name(str_parent, ScikitMultiTaskLasso.__name__)),
+        TheanetsANNs(x_dim, y_dim, math.floor(x_dim / 2), combine_name(str_parent, TheanetsANNs.__name__)),
+        PyBrainANNs(x_dim, y_dim, math.floor(x_dim / 2), combine_name(str_parent, PyBrainANNs.__name__))
+    ]
 
 
 class RegressionManager:
-    def __init__(self, model_list, s_path):
-        self.serialize_path = s_path
+    def __init__(self, model_list):
         self.model_list = model_list
         self.scalar = None
 
@@ -38,8 +60,23 @@ class RegressionManager:
 
         for model in self.model_list:
             model.train(x_train_scaled, y_train)
-            model.save(os.path.join(self.serialize_path, model.serialize_id))
+
+    def save(self, path):
+        for model in self.model_list:
+            model.save(os.path.join(path, model.serialize_id))
+
+    def load(self, path):
+        for model in self.model_list:
+            model.load(os.path.join(path, model.serialize_id))
 
     def predict(self, x_data):
         x_data_scaled = self.scalar.transform(x_data)
         return [m.predict(x_data_scaled) for m in self.model_list]
+
+    @classmethod
+    def color_regression(cls):
+        return cls(_register_color_regression())
+
+    @classmethod
+    def quality_regression(cls, x_dim, y_dim):
+        return cls(_register_quality_regression(x_dim, y_dim))
--- src/main.py
+++ src/main.py
@@ -9,51 +9,60 @@ from src.file_io import SampleManager
 from src.file_io import PredictorManager
 from src.file_io import PublicSupport
 from src.image_feature import FeatureManager
-from src.learning.strategy import MixedRegression
-from src.learning.strategy import QualityRegression
-from src.learning.strategy import ColorRegression
-from src.learning.strategy.RegressionManager import feature_dimensions
-
+from src.learning.strategy import RegressionManager
 
 __author__ = 'Kern'
 
 
-def feature(images_data, feature_name):
+def feature_dimensions(data_struct):
+    if len(data_struct.shape) >= 2:
+        return data_struct.shape[1]
+    elif len(data_struct.shape) == 1:
+        return 1
+
+    raise TypeError('unknown pandas struct type')
+
+
+def feature(images_data, feature_name, feature_data_path):
     # feature extraction
     feats_list = [FeatureManager.compute_feats(img).append(info) for img, info in images_data]
     feat_df = pd.DataFrame(feats_list)
-    PublicSupport.save_dataframe(feat_df, os.path.join(feature_data_home, feature_name + datetime.now().strftime("%Y-%m-%d %H.%M.%S")))
+    PublicSupport.save_dataframe(feat_df, os.path.join(feature_data_path, feature_name + datetime.now().strftime("%Y-%m-%d %H.%M.%S")))
 
     return feat_df
 
 
-def calc_features(b_training):
-    if b_training:
-        # load file and image preprocessing
-        excel_dataframe = (pd.read_excel(excel_file, excel_sheet_name, index_col=None, na_values=['NA'])).dropna(axis=0)
-        preprocessed_path = os.path.join(original_data_home, preprocessed_folder)
-        create_path(preprocessed_path)
-        image_dict = SampleManager.prepare_preprocessing_image(excel_dataframe, preprocessed_path, original_data_home, file_column_name)
-        training_data = SampleManager.prepare_training_data(excel_dataframe, image_dict, file_column_name, color_column_name, quality_column_name, subjective_column_name)
-        feat_df = feature(training_data, 'feature_train')
-    else:
-        # load file and image preprocessing
-        preprocessed_path = os.path.join(predict_data_home, preprocessed_folder)
-        create_path(preprocessed_path)
-        image_dict = PredictorManager.prepare_preprocessing_image(preprocessed_path, predict_data_home, "*.jpg")
-        prediction_data = PredictorManager.prepare_training_data(image_dict, file_column_name, subjective_column_name)
-        feat_df = feature(prediction_data, 'feature_prediction')
+def calc_train_features(original_data_path, preprocessed_dir, excel, sheet_name, feature_data_path):
+    excel_dataframe = (pd.read_excel(excel, sheet_name, index_col=None, na_values=['NA'])).dropna(axis=0)
+    preprocessed_dir = os.path.join(original_data_path, preprocessed_dir)
+    create_path(preprocessed_dir)
+    image_dict = SampleManager.prepare_preprocessing_image(excel_dataframe, preprocessed_dir, original_data_path, file_column_name)
+    training_data = SampleManager.prepare_training_data(excel_dataframe, image_dict, file_column_name, color_column_name, quality_column_name, subjective_column_name)
+    return feature(training_data, 'feature_train', feature_data_path)
 
-    return feat_df
+
+def calc_predict_features(predict_data_path, preprocessed_dir, feature_data_path):
+    preprocessed_path = os.path.join(predict_data_path, preprocessed_dir)
+    create_path(preprocessed_path)
+    image_dict = PredictorManager.prepare_preprocessing_image(preprocessed_path, predict_data_path, "*.jpg")
+    prediction_data = PredictorManager.prepare_training_data(image_dict, file_column_name, subjective_column_name)
+    return feature(prediction_data, 'feature_prediction', feature_data_path)
 
 
 def load_features(feat_path, pattern):
     return PublicSupport.load_dataframe(PublicSupport.find_newest_file(feat_path, pattern))
 
 
-def train():
-    # feat_df = calc_features(True)
-    feat_df = load_features(feature_data_home, '*train*.csv')
+def load_models(model_list, model_data_path):
+    mng = RegressionManager.RegressionManager(model_list)
+    mng.load(model_data_path)
+
+    return mng
+
+
+def train(original_data_path, preprocessed_dir, excel, sheet_name, feature_data_path, model_data_path):
+    feat_df = calc_train_features(original_data_path, preprocessed_dir, excel, sheet_name, feature_data_path)
+    # feat_df = load_features(feature_data_home, '*train*.csv')
 
     x_data = feat_df.drop(subjective_column_name, axis=1, level=0)
     y_data = feat_df[subjective_column_name]
@@ -61,26 +70,29 @@ def train():
     # simple linear models to train on color score
     color_x = x_data[hue_column_name]
     color_y = y_data[color_column_name]
-    color_models = ColorRegression.ColorRegression(model_data_home)
+    color_models = RegressionManager.RegressionManager(RegressionManager.register_color_regression())
     color_models.train(color_x, color_y)
+    color_models.save(model_data_path)
 
     # kinds of models to train on quality score
     quality_x = x_data
     quality_y = y_data[quality_column_name]
-    quality_models = QualityRegression.QualityRegression(feature_dimensions(quality_x), feature_dimensions(quality_y), model_data_home)
+    quality_models = RegressionManager.RegressionManager(RegressionManager.register_quality_regression(feature_dimensions(quality_x), feature_dimensions(quality_y)))
     quality_models.train(quality_x, quality_y)
+    quality_models.save(model_data_path)
 
     # modes to train on both color and quality
     mixed_x = x_data
     mixed_y = y_data[[color_column_name, quality_column_name]]
-    mixed_models = MixedRegression.MixedRegression(feature_dimensions(mixed_x), feature_dimensions(mixed_y), model_data_home)
+    mixed_models = RegressionManager.RegressionManager(RegressionManager.register_mixed_regression(feature_dimensions(mixed_x), feature_dimensions(mixed_y)))
     mixed_models.train(mixed_x, mixed_y)
+    mixed_models.save(model_data_path)
 
     return color_models, quality_models, mixed_models
 
 
-def predict(color_models, quality_models, mixed_models):
-    feat_df = calc_features(False)
+def predict(predict_data_path, preprocessed_dir, feature_data_path, color_models, quality_models, mixed_models):
+    feat_df = calc_predict_features(predict_data_path, preprocessed_dir, feature_data_path)
     # feat_df = load_features(feature_data_home, '*prediction*.csv')
 
     x_data = feat_df.drop(subjective_column_name, axis=1, level=0)
@@ -126,4 +138,7 @@ quality_column_name = json_dict['quality_column_name']
 subjective_column_name = json_dict['subjective_column_name']
 hue_column_name = json_dict['hue_column_name']
 
-predict(*train())
+
+# color_m = load_models(RegressionManager.register_color_regression())
+color_m, quality_m, mixed_m = train(original_data_home, preprocessed_folder, excel_file, excel_sheet_name, feature_data_home, model_data_home)
+predict(predict_data_home, preprocessed_folder, feature_data_home, color_m, quality_m, mixed_m)
